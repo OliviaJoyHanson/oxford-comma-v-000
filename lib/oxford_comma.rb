28fa84d31f3b9846da3ffd9be61ec6b5a5982254
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-04-20 15:31:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,8 @@ def oxford_comma(array)
     array.push(no_oxford).join
   elsif array.size > 2
     last = array.pop
-    with_and = " and " << last
-    array << with_and
+    with_and = "and " << last
+    array.with_and
     array.join(", ")
   elsif array.size == 1
     array.join
